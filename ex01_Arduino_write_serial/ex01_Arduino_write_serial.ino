0d1de1066944a96339e5d57df0bdd8bfbc6684f5
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2024-11-15 01:41:47 -0500

    edited arduino processing example

--- ex01_Arduino_write_serial/ex01_Arduino_write_serial.ino
+++ ex01_Arduino_write_serial/ex01_Arduino_write_serial.ino
@@ -1,14 +1,18 @@
 // Code to read an analog value, in this case from a potentiometer and write it to the serial port
-
-int val;
-int inputPin = 0;                // Set the input to analog in pin 0
+int inputPin = A0;                // Set the input to analog in pin 0
 
 void setup() {
   Serial.begin(9600);            // Start serial communication at 9600 bps
 }
 
 void loop() {
-  val = analogRead(inputPin)/4;  // Read analog input pin, put in range 0 to 255
-  Serial.write(val);             // Send the value
+  //Serial.println(analogRead(inputPin));
+  //sensorReading = analogRead(inputPin);
+  int sensorReading = analogRead(inputPin);
+  int mappedVal = map(sensorReading, 0, 1023, 0, 255); //analogRead(inputPin) / 4;  // Read analog input pin, put in range 0 to 255
+  //Serial.println(mappedVal);
+  
+  Serial.write(mappedVal);             // Send the value
+  //Serial.println(mappedVal);
   delay(100);                    // give the A2D converter a moment to catch up
 }
