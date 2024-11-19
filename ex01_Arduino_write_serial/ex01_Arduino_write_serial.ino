c706a09717ab5a3d3f17dcfae17993b8f078cfcf
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2024-11-19 04:03:38 -0500

    changed serial write method to println

--- ex01_Arduino_write_serial/ex01_Arduino_write_serial.ino
+++ ex01_Arduino_write_serial/ex01_Arduino_write_serial.ino
@@ -1,18 +1,18 @@
 // Code to read an analog value, in this case from a potentiometer and write it to the serial port
-int inputPin = A0;                // Set the input to analog in pin 0
+int inputPin = A5;                // Set the input to analog
 
 void setup() {
   Serial.begin(9600);            // Start serial communication at 9600 bps
+  //Serial.println("hi");
 }
 
 void loop() {
   //Serial.println(analogRead(inputPin));
-  //sensorReading = analogRead(inputPin);
   int sensorReading = analogRead(inputPin);
   int mappedVal = map(sensorReading, 0, 1023, 0, 255); //analogRead(inputPin) / 4;  // Read analog input pin, put in range 0 to 255
-  //Serial.println(mappedVal);
   
-  Serial.write(mappedVal);             // Send the value
-  //Serial.println(mappedVal);
+  //Serial.write(mappedVal);             // Send the value
+  Serial.print("RATE");
+  Serial.println(mappedVal);
   delay(100);                    // give the A2D converter a moment to catch up
 }
