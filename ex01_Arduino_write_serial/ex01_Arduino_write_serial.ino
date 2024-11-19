3ae26930528d5ed8d39c410f46711d7dbc80874f
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2024-11-19 05:50:08 -0500

    finished exp7 - connected to second potentiometer for rotation

--- ex01_Arduino_write_serial/ex01_Arduino_write_serial.ino
+++ ex01_Arduino_write_serial/ex01_Arduino_write_serial.ino
@@ -1,18 +1,30 @@
 // Code to read an analog value, in this case from a potentiometer and write it to the serial port
-int inputPin = A5;                // Set the input to analog
+int input1 = A5;
+int input2 = A4;
 
 void setup() {
-  Serial.begin(9600);            // Start serial communication at 9600 bps
-  //Serial.println("hi");
+  Serial.begin(9600); // Start serial communication at 9600 bps
 }
 
 void loop() {
-  //Serial.println(analogRead(inputPin));
-  int sensorReading = analogRead(inputPin);
-  int mappedVal = map(sensorReading, 0, 1023, 0, 255); //analogRead(inputPin) / 4;  // Read analog input pin, put in range 0 to 255
+  //Serial.println(analogRead(input1));
+
+  // Read analog input pin for sample rate, put in range 0 to 255
+  int reading1 = analogRead(input1);
+  int rateVal = map(reading1, 0, 1023, 0, 255);
   
-  //Serial.write(mappedVal);             // Send the value
+  //Serial.write(rateVal); 
+  // Send the value
   Serial.print("RATE");
-  Serial.println(mappedVal);
-  delay(100);                    // give the A2D converter a moment to catch up
+  Serial.println(rateVal);
+
+  // Read analog input pin, put in range 0 to 255
+  int reading2 = analogRead(input2);
+  int rotationVal = map(reading2, 0, 1023, 0, 255);
+  
+  // Send the value
+  Serial.print("ROT");
+  Serial.println(rotationVal);
+
+  delay(100); // give the A2D converter a moment to catch up
 }
